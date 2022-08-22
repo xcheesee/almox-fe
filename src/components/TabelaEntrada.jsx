f1dbd5adcfff89861e598c8430e1adcf3d3ddb04
Deivison Leite de Macedo <deivisonmacedo@prefeitura.sp.gov.br>
2022-08-22 14:42:00 -0300

    Adição do botão de listar materiais

--- src/components/TabelaEntrada.jsx
+++ src/components/TabelaEntrada.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import { mascaraProcessoSei, mascaraContrato } from '../helpers';
-import { 
-    TableRow, 
-    TableCell, 
+import {
+    TableRow,
+    TableCell,
     IconButton,
     Tooltip
 } from '@mui/material';
 import Tabela from './Tabela';
 import ManageSearchIcon from '@mui/icons-material/ManageSearch';
 import EditIcon from '@mui/icons-material/Edit';
+import FormatListNumberedIcon from '@mui/icons-material/FormatListNumbered';
 
 const cabecalhos = [
     "ID",
@@ -21,11 +22,11 @@ const cabecalhos = [
 ];
 
 const entradas = [
-    {id: 1, data: "2022-08-15 14:09", processo_sei: "1234202212345678", contrato: "001SVMA2022", local: "Base Leolpoldina", nota_fiscal: "12341234123"},
-    {id: 2, data: "2022-07-20 16:16", processo_sei: "4321202276543210", contrato: "002SVMA2022", local: "Base Leolpoldina", nota_fiscal: "43214321432"},
-    {id: 3, data: "2022-02-13 18:32", processo_sei: "6000202276541239", contrato: "003SVMA2022", local: "Base Leolpoldina", nota_fiscal: "12346784466"},
-    {id: 4, data: "2022-04-17 10:35", processo_sei: "6066202264315275", contrato: "004SVMA2022", local: "Base Leolpoldina", nota_fiscal: "13456789578"},
-    {id: 5, data: "2022-09-01 12:21", processo_sei: "6123202294632157", contrato: "005SVMA2022", local: "Base Leolpoldina", nota_fiscal: "76453145692"},
+    { id: 1, data: "2022-08-15 14:09", processo_sei: "1234202212345678", contrato: "001SVMA2022", local: "Base Leolpoldina", nota_fiscal: "12341234123" },
+    { id: 2, data: "2022-07-20 16:16", processo_sei: "4321202276543210", contrato: "002SVMA2022", local: "Base Leolpoldina", nota_fiscal: "43214321432" },
+    { id: 3, data: "2022-02-13 18:32", processo_sei: "6000202276541239", contrato: "003SVMA2022", local: "Base Leolpoldina", nota_fiscal: "12346784466" },
+    { id: 4, data: "2022-04-17 10:35", processo_sei: "6066202264315275", contrato: "004SVMA2022", local: "Base Leolpoldina", nota_fiscal: "13456789578" },
+    { id: 5, data: "2022-09-01 12:21", processo_sei: "6123202294632157", contrato: "005SVMA2022", local: "Base Leolpoldina", nota_fiscal: "76453145692" },
 ];
 
 const TabelaEntrada = () => {
@@ -46,9 +47,14 @@ const TabelaEntrada = () => {
                                     <ManageSearchIcon />
                                 </IconButton>
                             </Tooltip>
-                            <Tooltip title="Editar" placement="right">
+                            <Tooltip title="Editar" placement="top">
                                 <IconButton>
                                     <EditIcon />
+                                </IconButton>
+                            </Tooltip>
+                            <Tooltip title="Listar materiais" placement="right">
+                                <IconButton>
+                                    <FormatListNumberedIcon />
                                 </IconButton>
                             </Tooltip>
                         </TableCell>
