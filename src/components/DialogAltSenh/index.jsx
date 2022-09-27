2d6e6aee6fc1ba544287b564d4cc1223403789ff
João Lucas Silva Laurindo Cruz <x529427@rede.sp>
2022-09-27 12:10:22 -0300

    implementacao inicial de troca de senha

--- src/components/DialogAltSenh/index.jsx
+++ src/components/DialogAltSenh/index.jsx
@@ -19,97 +19,98 @@ const DialogAltSenh = ({openAltSenha, setOpenAltSenha, carregando, setCarregando
 
     return(
         <Dialog open={openAltSenha} fullWidth={reqResponse === ''} maxWidth="md">
-            
-           { reqResponse === ''
-           ? <>
-               <DialogTitle>
-                    <Tooltip title="Voltar">
-                        <IconButton onClick={() => setOpenAltSenha(false)}>
-                            <ArrowBackIosNewIcon fontSize="small" />
-                        </IconButton>
-                    </Tooltip>
-                    Alterar Senha
-                </DialogTitle>
-                <DialogContent>
-                    <Box
-                        sx={style.gridAlt}
-                        component="form"
-                        id="senha"
-                        name="senha"
-                        onSubmit={async (e) => {
-                            e.preventDefault()
-                            const formData = Object.fromEntries(new FormData(e.target))
-                            console.log(formData)
-                            setCarregando(true)
-                            const res = await pwRequest(formData)
-                            setReqResponse(res)
-                            setCarregando(false)         
-                        }}
-                    >
-                        <TextField
-                            label="Email"
-                            name="email"
-                            id="email"
-                            required
-                        />
-                        <TextField
-                            label="Senha Atual"
-                            name="password"
-                            id="password"
-                            required
-                        />
-                        <TextField
-                            label="Nova Senha"
-                            name="newpassword"
-                            id="newpassword"
-                            required
-                        />
-                        <TextField
-                            name="password_confirmation"
-                            label="Confirmação Nova Senha"
-                            id="password_confirmation"
-                            required
-                        />
-                    </Box>
-                </DialogContent>
-                <DialogActions sx={{ justifyContent: 'space-between' , padding: "0 0 1rem 2rem"}}>
-                    <Box sx={{ display: 'flex', margin: '0.5rem', gap: '1rem' }}>
-                        <Button
-                            type="submit"
-                            form="senha"
-                            variant="contained" sx={{ gap: '0.5rem' }}
-                        >
-                            {carregando
-                                ? <CircularProgress color="color" size="1rem" />
-                                : ''
-                            }
-                            Salvar
-                        </Button>
-                    </Box>
-                </DialogActions>
-           </>
-            :
-            <>
-                <DialogContent>{reqResponse.message}</DialogContent>
-                <DialogActions sx={{ justifyContent: 'center'}}>
-                    <Box sx={{ display: 'flex', margin: '0.5rem', gap: '1rem' }}>
-                        <Button
-                            onClick={async () => {
-                                setOpenAltSenha(false)
-                                //timeout para evitar flicker de dialog
-                                await(new Promise((res, rej) => {
-                                    setTimeout(() => res('true'), 500)
-                                }))
-                                setReqResponse('')
-                            }}
-                            form="senha"
-                            variant="contained" sx={{ gap: '0.5rem' }}
-                        >Ok</Button>
-                    </Box>
-                </DialogActions>
-            </>
+           {reqResponse === ''
+                ?<>
+                    <DialogTitle>
+                            <Tooltip title="Voltar">
+                                <IconButton onClick={() => setOpenAltSenha(false)}>
+                                    <ArrowBackIosNewIcon fontSize="small" />
+                                </IconButton>
+                            </Tooltip>
+                            Alterar Senha
+                        </DialogTitle>
+                        <DialogContent>
+                            <Box
+                                sx={style.gridAlt}
+                                component="form"
+                                id="senha"
+                                name="senha"
+                                onSubmit={async (e) => {
+                                    e.preventDefault()
+                                    const formData = Object.fromEntries(new FormData(e.target))
+                                    console.log(formData)
+                                    setCarregando(true)
+                                    const res = await pwRequest(formData)
+                                    setReqResponse(res)
+                                    setCarregando(false)         
+                                }}
+                            >
+                                <TextField
+                                    label="Email"
+                                    name="email"
+                                    id="email"
+                                    required
+                                />
+                                <TextField
+                                    label="Senha Atual"
+                                    name="password"
+                                    id="password"
+                                    type="password"
+                                    required
+                                />
+                                <TextField
+                                    label="Nova Senha"
+                                    name="newpassword"
+                                    type="password"
+                                    id="newpassword"
+                                    required
+                                />
+                                <TextField
+                                    name="password_confirmation"
+                                    label="Confirmação Nova Senha"
+                                    type="password"
+                                    id="password_confirmation"
+                                    required
+                                />
+                            </Box>
+                        </DialogContent>
+                        <DialogActions sx={{ justifyContent: 'space-between' , padding: "0 0 1rem 2rem"}}>
+                            <Box sx={{ display: 'flex', margin: '0.5rem', gap: '1rem' }}>
+                                <Button
+                                    type="submit"
+                                    form="senha"
+                                    variant="contained" sx={{ gap: '0.5rem' }}
+                                >
+                                    {carregando
+                                        ? <CircularProgress color="color" size="1rem" />
+                                        : ''
+                                    }
+                                    Salvar
+                                </Button>
+                            </Box>
+                        </DialogActions>
+                </>
+                :<>
+                    <DialogContent>{reqResponse.message}</DialogContent>
+                    <DialogActions sx={{ justifyContent: 'center'}}>
+                        <Box sx={{ display: 'flex', margin: '0.5rem', gap: '1rem' }}>
+                            <Button
+                                onClick={async () => {
+                                    setOpenAltSenha(false)
+                                    //timeout para evitar flicker de dialog
+                                    await(new Promise((res, rej) => {
+                                        setTimeout(() => res('true'), 500)
+                                    }))
+                                    setReqResponse('')
+                                }}
+                                variant="contained" sx={{ gap: '0.5rem' }}
+                            >
+                                Ok
+                            </Button>
+                        </Box>
+                    </DialogActions>
+                </>
             }
-
         </Dialog>
     )
 }
