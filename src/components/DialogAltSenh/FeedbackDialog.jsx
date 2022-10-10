72522aaa4d178beb623f084aba9926d903592374
Deivison Leite de Macedo <deivisonmacedo@prefeitura.sp.gov.br>
2022-10-10 16:44:03 -0300

    Alterações no layout do dialog de mensagem de alteração de senha

--- src/components/DialogAltSenh/FeedbackDialog.jsx
+++ src/components/DialogAltSenh/FeedbackDialog.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { 
-    Box,
     DialogContent, 
+    DialogContentText,
     DialogActions, 
     Button,
 } from '@mui/material';
@@ -9,23 +9,24 @@ import {
 const FeedbackDialog = ({reqResponse, setOpenAltSenha, setReqResponse}) => {
     return (
         <>
-            <DialogContent>{reqResponse.message}</DialogContent>
-            <DialogActions sx={{ justifyContent: 'center'}}>
-                <Box sx={{ display: 'flex', margin: '0.5rem', gap: '1rem' }}>
-                    <Button
-                        onClick={async () => {
-                            setOpenAltSenha(false)
-                            //timeout para evitar flicker de dialog
-                            await(new Promise((res, rej) => {
-                                setTimeout(() => res('true'), 500)
-                            }))
-                            setReqResponse('')
-                        }}
-                        variant="contained" sx={{ gap: '0.5rem' }}
-                    >
-                        Ok
-                    </Button>
-                </Box>
+            <DialogContent>
+                <DialogContentText>
+                    {reqResponse.message}
+                </DialogContentText>
+            </DialogContent>
+            <DialogActions sx={{ display: 'flex', justifyContent: 'flex-end', }}>
+                <Button
+                    onClick={async () => {
+                        setOpenAltSenha(false)
+                        //timeout para evitar flicker de dialog
+                        await(new Promise((res, rej) => {
+                            setTimeout(() => res('true'), 500)
+                        }))
+                        setReqResponse('')
+                    }}
+                >
+                    OK
+                </Button>
             </DialogActions>
         </>
     )
