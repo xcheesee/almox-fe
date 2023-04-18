4afeb05c93c281711a29bbf407d539c6d14bc044
Joao Lucas Silva <joaolucas@prefeitura.sp.gov.br>
2023-04-18 10:11:55 -0300

    criado restricoes para adicao e edicao de itens de materiais em O.S

--- src/components/MatListCard/index.jsx
+++ src/components/MatListCard/index.jsx
@@ -1,17 +1,22 @@
-import { InputAdornment, MenuItem, Paper, TextField, Typography } from "@mui/material";
+import { InputAdornment, MenuItem, MenuList, Paper, TextField, Typography } from "@mui/material";
 import { Box } from "@mui/system";
 import { IconButton } from "@mui/material";
 import DeleteIcon from '@mui/icons-material/Delete';
 import { useQuery } from "@tanstack/react-query";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export default function MatListCard({ tipo, mats, getMats, modSelectedMat, delSelectedMat }) {
   const [allMats, setAllMats] = useState([])
+  const [currMats, setCurrMats] = useState(mats)
+  const [qtdError, setQtdError] = useState({})
+
   const matsQuery = useQuery({ 
     queryKey: ['mats', tipo], 
     queryFn: () => getMats(tipo.id), 
     onSuccess: res => setAllMats(res.data) 
   }) 
+
+  useEffect(() => setCurrMats(mats),[])
 
   const itemRef = useRef([])
   const qtdRef = useRef([])
@@ -20,19 +25,28 @@ export default function MatListCard({ tipo, mats, getMats, modSelectedMat, delSe
   return(
     <Paper className="p-4 grid grid-rows-[min-content_1fr] my-4">
       <Typography className="pb-4 capitalize" variant="h6">{tipo?.nome}</Typography>
-      { mats?.map( (ele, index) => (
+      { currMats?.map( (ele, index) => (
         <Box key={`tipo-${tipo.id}ele-${ele.id}i-${index}`} className="grid grid-cols-[1fr_1fr_min-content] gap-4 mb-4">
           <TextField
             select
             size="small"
             id="mat"
-            defaultValue={ele.id}
-            inputRef={ref => itemRef["current"][index] = ref}
+            onChange={(e) => {
+              let mat = allMats.find(ele => ele.id === e.target.value)
+              if (currMats.find(ele => ele.id === mat.id)) return
+              let mats = [...currMats]
+              mats[index] = mat
+              setCurrMats(mats)
+             //console.log(obj)
+            }}  
+            value={ele.id}
+            //inputRef={ref => itemRef["current"][index] = ref}
             label="Material"
             //sobrescreve o valor de quantidade total pois nao eh utilizado em card de modificacao
             onBlur={ () => {
-              if (mats.find(ele => ele.id === itemRef["current"][index].value)) return
-              modSelectedMat(allMats.find( ele => ele.id === itemRef["current"][index].value), tipo.id, index)
+              if (mats.find(ele => ele.id === currMats[index].id)) return
+              if (qtdError[index]) return
+              modSelectedMat(allMats.find( ele => ele.id === currMats[index].id), tipo.id, index)
             }}
 
           >
@@ -44,14 +58,20 @@ export default function MatListCard({ tipo, mats, getMats, modSelectedMat, delSe
           <TextField 
             label="Quantidade"
             id="qtd"
-            InputProps={{
-              endAdornment: <InputAdornment position="end">/ {ele.quantidade} {ele.medida}</InputAdornment> 
+            InputProps={{ endAdornment: <InputAdornment position="end">/ {ele.quantidade} {ele.medida}</InputAdornment> 
             }}
             inputRef={ ref => qtdRef["current"][index] = ref }
             defaultValue={ele.qtd}
+            error={qtdError[index]}
+            helperText={ qtdError[index] ? 'Quantidade usada não pode exceder a quantidade em estoque.' : '' }
             onBlur={ () => {
+              if(qtdRef["current"][index].value > ele.quantidade) {
+                setQtdError(prev => ({...prev, [index]: true}))
+                return
+              }
+              setQtdError(prev => ({...prev, [index]: false}))
               modSelectedMat({...ele, qtd: qtdRef["current"][index].value }, tipo.id, index)
-            }}
+            } }
             size="small"
           />
 
